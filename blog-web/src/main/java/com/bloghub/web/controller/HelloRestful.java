1061e051719d9a0abac759ad08fcea67a9509107
Lang <googlebye@aliyun.com>
2017-03-04 14:15:50 +0800

    搭建框架
    Signed-off-by: Lang <googlebye@aliyun.com>

--- blog-web/src/main/java/com/bloghub/web/controller/HelloRestful.java
+++ blog-web/src/main/java/com/bloghub/web/controller/HelloRestful.java
@@ -1,22 +1,24 @@
 package com.bloghub.web.controller;
 
-import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.google.common.collect.Maps;
 
 @RestController
 public class HelloRestful {
 
-	@RequestMapping("/hello")
-	public String hello(){
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	public String hello() {
 		return "hello";
 	}
-	
-	@RequestMapping("/helloMap")
-	public Map<String, String> helloMap(){
-		Map<String, String> map = new HashMap();
+
+	@RequestMapping(value = "/helloMap")
+	public Map<String, String> helloMap() {
+		Map<String, String> map = Maps.newHashMap();
 		map.put("name", "cys");
 		map.put("word", "hello");
 		return map;
